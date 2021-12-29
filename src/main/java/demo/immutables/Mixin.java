8fe1677348d4031f768cc00673a7467662a8fceb
Harald Wellmann <harald.wellmann@northdata.de>
2021-12-29 12:17:53 +0100

    cleanup

--- src/main/java/demo/immutables/Mixin.java
+++ src/main/java/demo/immutables/Mixin.java
@@ -3,9 +3,6 @@ package demo.immutables;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * This allows to mixin the old-style error/Warning stuff into immutable objects
- */
 public interface Mixin<T> {
 
     T withProperty(String key, Object value);
@@ -13,5 +10,4 @@ public interface Mixin<T> {
     default List<String> warnings() {
         return (List<String>) Arrays.asList("foo", "bar");
     }
-
 }
